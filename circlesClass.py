f437a85cbe9b2f44bbaf1e1e60acf359f7c11587
Sebastian Mielczarek <s.mielczarek@clearcode.cc>
2021-11-21 16:29:25 +0100

    Ended list

--- circlesClass.py
+++ circlesClass.py
@@ -3,23 +3,25 @@ import random
 import pygame
 import math
 screen = pygame.display.set_mode((settings.SCREEN_W, settings.SCREEN_H))
-ay = -settings.g
-
+cx = settings.SCREEN_W/2
+cy = settings.SCREEN_H/2
 class Circle:
-    def __init__(self):
-        self.r = random.randint(10, settings.CIRCLE_MAX_RADIUS)
-        self.Vx = random.uniform(-settings.CIRCLE_MAX_SPEED,settings.CIRCLE_MAX_SPEED)
-        self.Vy = random.uniform(-settings.CIRCLE_MAX_SPEED,settings.CIRCLE_MAX_SPEED)
-
+    def __init__(self,r=None,Vx=None,Vy=None,x=None,y=None,c=None,w=None):
+        self.r = r if r is not None else random.randint(10, settings.CIRCLE_MAX_RADIUS)
+        self.Vx = Vx if Vx is not None else random.uniform(-settings.CIRCLE_MAX_SPEED,settings.CIRCLE_MAX_SPEED)
+        self.Vy = Vy if Vy is not None else random.uniform(-settings.CIRCLE_MAX_SPEED,settings.CIRCLE_MAX_SPEED)
         self.xBorderProtect = self.r+settings.BORDER_PADDING+settings.FPS+math.ceil(self.Vx)
         self.yBorderProtect = self.r+settings.BORDER_PADDING+settings.FPS+math.ceil(self.Vy)
-
-        self.x = random.randint(self.xBorderProtect,settings.SCREEN_W-(self.xBorderProtect))
-        self.y = random.randint(self.yBorderProtect,settings.SCREEN_H-(self.yBorderProtect))
-        self.c = [random.randint(0,255),random.randint(0,255),random.randint(0,255)]
-        self.w = self.r*1/4
+        self.x = x if x is not None else random.randint(self.xBorderProtect,settings.SCREEN_W-(self.xBorderProtect))
+        self.y = y if y is not None else random.randint(self.yBorderProtect,settings.SCREEN_H-(self.yBorderProtect))
+        self.c = c if c is not None else [random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+        self.w = w if w is not None else self.r
         self.Vypom = 0
-        self.F = self.w*-ay*(720-self.y)
+        self.Fx = 0 
+        self.Fy = 0
+        self.Dx = 0 
+        self.Dy = 0  
+
     def drawCircle(self):
         pygame.draw.circle(screen, self.c,(self.x,self.y),self.r)
 
@@ -27,17 +29,52 @@ class Circle:
         print(self.r,self.x,self.y,self.c)
 
     def moveCircles(self,dT):
-            self.x = self.x + self.Vx * dT
-            self.y = self.y + self.Vy * dT
+        self.x = self.x + self.Vx 
+        self.y = self.y + self.Vy 
+        self.checkCollision(dT)
 
-            self.Vy  = (self.Vy  - ay*self.w * dT)
-
-            if (self.y - self.r) + self.Vy *dT< 0 or self.y +self.r + self.Vy*dT > settings.SCREEN_H:
-                self.Vy = -self.Vy*0.9
-            if self.x-self.r + self.Vx*dT < 0 or self.x+self.r + self.Vx*dT > settings.SCREEN_W:
-                self.Vx = -self.Vx
-
-
+    def moveCirclesGravity(self,dT):
+        self.x = self.x + self.Vx * dT
+        self.y = self.y + self.Vy * dT
+        if self.y+self.r > settings.SCREEN_H*0.96:
+            self.Vy  = (self.Vy  + settings.g * self.w * dT)*0.9
+        else:
+            self.Vy  = (self.Vy  + settings.g * self.w * dT)*0.99
+        self.checkCollision(dT)
         
+    def moveCirclesMassCenter(self,dT):
+        pygame.draw.circle(screen,[0,0,0],(math.ceil(settings.SCREEN_W/2),math.ceil(settings.SCREEN_H/2)),50)
+        self.R = math.sqrt(pow((cy-self.y),2)+pow((cx-self.x),2))
+        self.Fy = -1*((settings.G*settings.CENTER_MASS_WEIGHT*self.w*(self.y-cy))/pow(self.R,3))
+        self.Fx = -1*((settings.G*settings.CENTER_MASS_WEIGHT*self.w*(self.x-cx))/pow(self.R,3))
+
+        self.Vx += self.Fx
+        self.Vy += self.Fy
+        
+        if self.x > settings.SCREEN_W or self.x <= 0 and self.y >= settings.SCREEN_H or self.y < 0:
+            self.x += self.Vx+(cx-self.x)*0.005
+            self.y += self.Vy+(cy-self.y)*0.005
+        else: 
+            self.x += self.Vx
+            self.y += self.Vy
+
+    def moveCirclesAerodynamics(self,dT):
+        #Metanol	0,587 η / mPa*s w temp. 20°C
+        self.Dx = -6 * math.pi * (0.587 * 0.001 * dT) * self.r * self.Vx
+        self.Dy = -6 * math.pi * (0.587 * 0.001 * dT) * self.r * self.Vy
+        
+        self.Vx += self.Dx
+        self.Vy += self.Dy
+
+        self.x += self.Vx
+        self.y += self.Vy
+
+        self.checkCollision(dT)
+
     def checkCollision(self,dT):
-        pass
+        if (self.y - self.r) + self.Vy * dT < 0 or self.y +self.r + self.Vy*dT > settings.SCREEN_H:
+            self.Vy = -self.Vy
+        if self.x-self.r + self.Vx*dT < 0 or self.x+self.r + self.Vx*dT > settings.SCREEN_W:
+            self.Vx = -self.Vx
+
+
--- main.py
+++ main.py
@@ -2,30 +2,136 @@ import settings
 from circlesClass import Circle
 from circlesClass import screen as screen
 import pygame
+import pygame_gui
 import random as rd 
-
+import math
 disks = []
 
 pygame.init()
+manager = pygame_gui.UIManager((settings.SCREEN_W, settings.SCREEN_H))
+
 clock = pygame.time.Clock()
 
+items = ["Normal","Gravity","Central mass","Aerodynamics"]
+
+diskNumberSlider = pygame_gui.elements.UIHorizontalSlider(relative_rect=pygame.Rect((0,settings.SCREEN_H/20),(settings.SCREEN_W/5,settings.SCREEN_H/20)),
+                                            value_range=(1,1000),
+                                            start_value=100,
+                                            manager=manager)
+diskNumberSliderLabel = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((0,0),(settings.SCREEN_W/5,settings.SCREEN_H/20)),
+                                            text="Disk Number: 1-1000",
+                                            parent_element=diskNumberSlider,
+                                            manager=manager)
+
+animationTypeSelectList = pygame_gui.elements.UISelectionList(relative_rect=pygame.Rect((settings.SCREEN_W/5,0),(settings.SCREEN_W/5,settings.SCREEN_H/10)),
+                                            item_list=items,
+                                            allow_multi_select=False,
+                                            manager=manager)
+
+hideUI = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((settings.SCREEN_W-(settings.SCREEN_W/5),0),(settings.SCREEN_W/5,settings.SCREEN_H/20)),
+                                            text="Hide UI",
+                                            manager=manager)
+
+showUI = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((settings.SCREEN_W-(settings.SCREEN_W/5),0),(settings.SCREEN_W/5,settings.SCREEN_H/20)),
+                                            text="Show UI",
+                                            manager=manager)
+
+startButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((settings.SCREEN_W-(2*(settings.SCREEN_W/5)),0),(settings.SCREEN_W/5,settings.SCREEN_H/20)),
+                                            text="Start",
+                                            manager=manager)
+
+stopButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((settings.SCREEN_W-(2*(settings.SCREEN_W/5)),0),(settings.SCREEN_W/5,settings.SCREEN_H/20)),
+                                            text="Stop",
+                                            manager=manager)
+
 screen.fill((255,255,255))
-
-for x in range(settings.CIRCLES_NUMBER):
-    disks.append(Circle())
-    disks[x].drawCircle()
-    pygame.display.update()  
     
-while True:
+showUI.hide()
+stopButton.hide()
+is_running = True
+run_animation = False
+
+while is_running:
+    dT = clock.tick(30) * 0.0001 * settings.FPS
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:  
-            pygamme.quit()
+        if event.type == pygame.QUIT:
+            is_running = False
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == hideUI:
+                    diskNumberSlider.hide()
+                    diskNumberSliderLabel.hide()
+                    animationTypeSelectList.hide()
+                    hideUI.hide()
+                    showUI.show()
+                    if run_animation == False:
+                        startButton.hide()
+                    else:
+                        stopButton.hide()
+                    
+                    screen.fill((255,255,255))
 
-    dT = clock.tick(30) * .0001 * settings.FPS
-    screen.fill((255,255,255))
 
-    for disk in disks:
-        disk.moveCircles2(dT)
-        disk.drawCircle()  
-        
+                if event.ui_element == showUI:
+                    diskNumberSlider.show()
+                    diskNumberSliderLabel.show()
+                    animationTypeSelectList.show()
+                    hideUI.show()
+                    showUI.hide()
+                    if run_animation == False:
+                        startButton.show()
+                        stopButton.hide()
+                    else:
+                        stopButton.show()
+                        startButton.hide()
+
+                if event.ui_element == startButton:
+                    run_animation = True
+                    stopButton.show()
+                    startButton.hide()
+
+                if event.ui_element == stopButton:
+                    run_animation = False
+                    startButton.show()
+                    stopButton.hide()
+                    
+
+        manager.process_events(event)
+
+   
+    diskNumberValue = diskNumberSlider.get_current_value()
+    
+    if diskNumberValue < len(disks):
+        disks=disks[:-len(disks)-diskNumberValue]
+        screen.fill((255,255,255))
+        for disk in disks:
+            disk.drawCircle()
+    elif diskNumberValue > len(disks):
+        for _ in range(diskNumberValue-len(disks)):
+            disks.append(Circle())
+            disks[-1].drawCircle()
+    if run_animation == True:
+        screen.fill((255,255,255))
+        animationType = animationTypeSelectList.get_single_selection()
+        if animationType == "Gravity":
+            for disk in disks:
+                disk.moveCirclesGravity(dT)
+                disk.drawCircle()
+        elif animationType == "Central mass":
+            for disk in disks:
+                disk.moveCirclesMassCenter(dT)
+                disk.drawCircle()
+        elif animationType =="Aerodynamics":
+            for disk in disks:
+                disk.moveCirclesAerodynamics(dT)
+                disk.drawCircle()
+        else:
+            for disk in disks:
+                disk.moveCircles(dT)
+                disk.drawCircle()
+    else:
+        for disk in disks:
+                disk.drawCircle()
+    manager.update(dT)
+    manager.draw_ui(screen)
     pygame.display.flip()
